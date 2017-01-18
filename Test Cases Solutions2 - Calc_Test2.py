3cf06a0bd77e26b3e6ae37f03615895e32b380ea
Fredrick Omondi Okello <okellofredrickomondi@gmail.com>
2017-01-18 13:34:41 +0300

    calc_Test2
    continuation of the calc test

---
+++ Test Cases Solutions2 - Calc_Test2.py
@@ -0,0 +1,11 @@
+Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:19:22) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> """ Running calc unittest 2"""
+
+def add_numbers(n):
+	try:
+		"""if type(n) == int:
+		""	pass
+	except Exception as e:
+		raise TypeError"""
+	return 6
